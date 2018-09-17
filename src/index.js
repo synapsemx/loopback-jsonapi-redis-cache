036e223727dda0cc64392d1ffc3c84b362d59920
Shane Ballman <shane.ballman@synapsemx.com>
2018-09-17 11:58:12 -0400

    Oy, whack a mole learning sucks

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { deprecate } from 'util';
-import rediscache from './rediscache';
+import redisCache from './redis-cache';
 
 export default deprecate(
-  app => app.loopback.modelBuilder.mixins.define('jsonapi-redis-mixin', rediscache),
+  app => app.loopback.modelBuilder.mixins.define('RedisCache', redisCache),
   'DEPRECATED: Add this mixin via the mixins array of model-config.json'
 );

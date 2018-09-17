18edbd5a4c15b158601001d089de88373f77e556
Shane Ballman <shane.ballman@synapsemx.com>
2018-09-17 10:24:39 -0400

    Initial support for JSON API payload

--- README.md
+++ README.md
@@ -1,33 +1,62 @@
-# loopback-redis-cache
-Redis cache mixin for loopback.io
+# loopback-jsonapi-redis-cache
+Mixin for Loopback that provides a Redis cache using the JSON API payload format.
+
+Builds on these great packages:
+
+ - [redis](https://github.com/NodeRedis/node_redis)
+ - [redis-delete-pattern](https://github.com/uber/redis-delete-pattern)
+ - [loopback-redis-cache](https://github.com/vkatsar/loopback-redis-cache)
+ - [loopback-jsonapi-model-serializer](https://github.com/digitalsadhu/loopback-jsonapi-model-serializer)
 
 # Features
 
-  - Cache every GET request using only one get option.
-  - Different Redis server for each model with fallback to config.json Redis server.
-  - Invalidate cache on every create, update or delete.
+  - Cache every GET request with the query param `cache={ttl}` (ie, `cache=3600`).
+  - Optional per-model Redis server with fallback to the default Redis server.
+  - Invalidate cache on every create, update, delete, or when TTL expires.
 
 ### Installation
 
-loopback-redis-cache requires [Node.js](https://nodejs.org/) v4+ to run.
+loopback-jsonapi-redis-cache requires [Node.js](https://nodejs.org/) v4+ to run.
 
  Install using npm
 
-```sh
-$ npm install loopback-redis-cache --save
 ```
-Add to /server/config.json
+$ npm install loopback-jsonapi-redis-cache --save
+```
+
+You can statically define things if you want...
+`/server/config.json`
 ```
   "redis": {
     "host": "127.0.0.1",
-    "password": "your-redis-password"
+    "port": "6379",
+    "password": "optional-redis-password"
   }
 ```  
+
+But we prefer to use Loopback's environment substitutions here, along with .env vars...
+
+`/server/config.development.js`
+
+```
+module.exports = {
+  ...
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD,
+  },
+  ...
+};
+
+```
+
+
 Add to /server/model-config.json
 ```
     "mixins": [
       ...
-      "../node_modules/loopback-redis-cache"
+      "../node_modules/loopback-jsonapi-redis-cache"
       ...
     ]
 ```
@@ -40,18 +69,21 @@ loopback-redis-cache is currently extended with the following plugins.
 | ------ | ------ |
 | redis | [https://github.com/NodeRedis/node_redis/blob/master/README.md] |
 | redis-delete-pattern | [https://github.com/uber-archive/redis-delete-pattern/blob/master/README.md] |
+| redis-delete-pattern | [https://github.com/uber-archive/redis-delete-pattern/blob/master/README.md] |
+| loopback-jsonapi-model-serializer | [https://github.com/digitalsadhu/loopback-jsonapi-model-serializer/blob/master/README.md] |
+
 
 ### How to use it
-At your model (using config.json settings)
+In each model you want to provide caching (using the default redis server)
 ```
   "mixins": {
-     "Rediscache": {}      
+     "RedisCache": {} // case sensitive!
   }
 ```  
-At your model (using external redis server)
+If you want to provide a per-model redis server (optional)
 ```
   "mixins": {
-     "Rediscache": {
+     "RedisCache": {
        "client": {
          "host": "redis.server.ip.address",
          "password": "redis-password"
@@ -59,15 +91,15 @@ At your model (using external redis server)
      }    
   }
   ```
-  
-  Example 
+
+  Example
     ``
   http://0.0.0.0:3000/api/games?cache=120
     ``
   cache value in seconds
-  
+
   ### AngularJS SDK example
-  
+
   ```
     Category.findOne({
         filter: {
@@ -75,11 +107,23 @@ At your model (using external redis server)
                 categoryId: $scope.Id
             }
         },
-        cache: 123
+        cache: 120
     }, function (item) {
         console.log(item);
     }, function (err) {
         console.log(err);
     });
   ```
-  
+
+  ### EmberJS example
+
+  ```
+    this.get('store').query(modelName, {
+          filter: {
+            where: {
+                categoryId: this.get('categoryId')
+              }
+          },
+          cache: 120
+        }).then()...
+  ```
--- rediscache.js
+++ rediscache.js
@@ -1,118 +1,143 @@
+/* Mixin name: 'RedisCache' (case-sensitive)
+ * Purpose: Cache every GET request with our cache query param
+ *          Invalidate cache on every create, update, delete, or ttl expiration
+ *
+ * Options: custom redis server per loopback model
+ *          'RedisCache': {
+ *             'client': {
+ *               'host': 'redis.server.ip.address',
+ *               'password': 'redis-password'
+ *             }
+ *           }
+ */
+const redis = require('redis');
+const redisDeletePattern = require('redis-delete-pattern');
+const serialize = require('loopback-jsonapi-model-serializer');
+const debug = require('debug')('loopback:mixin:jsonapi-redis-cache');
+
 module.exports = function(Model, options) {
-    if(options.client){
-        var clientSettings = options.client;
-    }else{
-        var app = require('../../server/server.js');
-        var clientSettings = app.get('redis');
+  var clientSettings;
+  if (options && options.client) {
+    clientSettings = options && options.client;
+  } else {
+    let rootApp = require('../../server/server.js');
+    clientSettings = rootApp.get('redis');
+  }
+  var client = redis.createClient(clientSettings);
+
+  function handleError(err) {
+    debug(err);
+    // try to connect again with server config
+    if (err.toString().indexOf('invalid password') !== -1) {
+      debug('Invalid password... reconnecting with server config...');
+      var app = require('../../server/server');
+      var clientSettings = app.get('redis');
+      client = redis.createClient(clientSettings);
     }
+  };
 
-    var redis = require("redis"),
-        client = redis.createClient(clientSettings);
+  client.on('error', handleError);
 
-    var redisDeletePattern = require('redis-delete-pattern'); 
+  Model.beforeRemote('**', function(ctx, res, next) {
+    // get all find methods and search first in cache
+    if ((ctx.method.name.indexOf('find') !== -1 ||
+      ctx.method.name.indexOf('__get') !== -1) && client.connected) {
+      if (typeof ctx.req.query.cache != 'undefined') {
+        var modelName = ctx.method.sharedClass.name;
+        var cacheExpire = ctx.req.query.cache;
+        let id = res.id || 'idx';
 
-    client.on("error", function (err) {
-        console.log(err);
-        // try to connect again with server config
-        if(err.toString().indexOf("invalid password") !== -1){
-            console.log("Invalid password... reconnecting with server config...");
-            var app = require('../../server/server');
-            var clientSettings = app.get('redis');        
-            client = redis.createClient(clientSettings);
-        }
-    });
+        // set key name
+        var cacheKey = modelName + '_' + id + '_' +
+          new Buffer(JSON.stringify(ctx.req.query)).toString('base64');
 
+        // search for cache
+        client.get(cacheKey, function(err, val) {
+          debug('***** cacheKey *****', cacheKey);
+          if (err) {
+            debug(err);
+          }
 
-    Model.beforeRemote('**', function(ctx, res, next) {
-        // get all find methods and search first in cache
-        if((ctx.method.name.indexOf("find") !== -1 || ctx.method.name.indexOf("__get") !== -1) && client.connected){
-            if(typeof ctx.req.query.cache != 'undefined'){
-                var modelName = ctx.method.sharedClass.name;
-                var cachExpire = ctx.req.query.cache;
-
-                // set key name
-                var cache_key = modelName+'_'+new Buffer(JSON.stringify(ctx.req.query)).toString('base64');
-
-                // search for cache
-                client.get(cache_key, function(err, val) {
-                    if(err){
-                        console.log(err);
-                    }
-
-                    if(val !== null){
-                        ctx.result = JSON.parse(val);
-                        ctx.done(function(err) {
-                            if (err) return next(err);
-                        });
-                    }else{
-                        //return data
-                        next();
-                    }                
-                });    
-
-            }else{
-                next();
-            }
-        }else{
-            next();
-        }            
-    });    
-
-    Model.afterRemote('**', function(ctx, res, next) {
-        // get all find methods and search first in cache - if not exist save in cache
-        if((ctx.method.name.indexOf("find") !== -1 || ctx.method.name.indexOf("__get") !== -1) && client.connected){
-            if(typeof ctx.req.query.cache != 'undefined'){
-                var modelName = ctx.method.sharedClass.name;
-                var cachExpire = ctx.req.query.cache;
-                
-                // set key name
-                var cache_key = modelName+'_'+new Buffer(JSON.stringify(ctx.req.query)).toString('base64');
-                // search for cache
-                client.get(cache_key, function(err, val) {
-                    if(err){
-                        console.log(err);
-                    }
-
-                    if(val == null){
-                        // set cache key
-                        client.set(cache_key, JSON.stringify(res));
-                        client.expire(cache_key, cachExpire);
-                        next();
-                    }else{
-                        next();
-                    }               
-                });    
-
-            }else{
-                next();
-            }
-        }else{
-            next();
-        }        
-    });
-
-    Model.afterRemote('**', function(ctx, res, next) {
-        // delete cache on patchOrCreate, create, delete, update, destroy, upsert
-        if((ctx.method.name.indexOf("find") == -1 && ctx.method.name.indexOf("__get") == -1) && client.connected){
-            var modelName = ctx.method.sharedClass.name;
-            var cachExpire = ctx.req.query.cache;
-            
-            // set key name
-            var cache_key = modelName+'_*';
-
-            // delete cache
-            redisDeletePattern({
-                redis: client,
-                pattern: cache_key
-            }, function handleError (err) {
-                if(err){
-                    console.log(err);
-                }
-                next();
+          if (val !== null) {
+            debug('***** val *****', JSON.parse(val));
+            ctx.result = JSON.parse(val);
+            ctx.done(function(err) {
+              if (err) return next(err);
             });
-
-        }else{
+          } else {
+            // return data
             next();
-        }    
-    });
-}
+          }
+        });
+      } else {
+        next();
+      }
+    } else {
+      next();
+    }
+  });
+
+  Model.afterRemote('**', function(ctx, res, next) {
+    // get all find methods and search first in cache - if not exist save in cache
+    if ((ctx.method.name.indexOf('find') !== -1 ||
+      ctx.method.name.indexOf('__get') !== -1) && client.connected) {
+      if (typeof ctx.req.query.cache != 'undefined') {
+        var modelName = ctx.method.sharedClass.name;
+        var cacheExpire = ctx.req.query.cache;
+        let id = res.id || 'idx';
+
+        // set key name
+        var cacheKey = modelName + '_' + id + '_' +
+         new Buffer(JSON.stringify(ctx.req.query)).toString('base64');
+
+        // search for cache
+        client.get(cacheKey, function(err, val) {
+          if (err) {
+            debug(err);
+          }
+
+          if (val == null) {
+            // set cache key
+            let _model = Model.app.models[`${modelName}`];
+            client.set(cacheKey,
+              JSON.stringify(serialize(res, _model))); // serialize for json-api
+            client.expire(cacheKey, cacheExpire);
+            next();
+          } else {
+            next();
+          }
+        });
+      } else {
+        next();
+      }
+    } else {
+      next();
+    }
+  });
+
+  Model.afterRemote('**', function(ctx, res, next) {
+    // delete cache on patchOrCreate, create, delete, update, destroy, upsert
+    if ((ctx.method.name.indexOf('find') == -1 &&
+      ctx.method.name.indexOf('__get') == -1) && client.connected) {
+      var modelName = ctx.method.sharedClass.name;
+      var cacheExpire = ctx.req.query.cache;
+      let id = res.id || 'idx';
+
+      // set key name
+      var cacheKey = modelName + '_' + id + '_*';
+
+      // delete cache
+      redisDeletePattern({
+        redis: client,
+        pattern: cacheKey,
+      }, function handleError(err) {
+        if (err) {
+          debug(err);
+        }
+        next();
+      });
+    } else {
+      next();
+    }
+  });
+};
